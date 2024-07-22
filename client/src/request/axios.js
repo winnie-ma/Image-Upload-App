f012df81755cde022540e885982172e0f49fab1a
winnie-ma <xuma8821@gmail.com>
2024-07-23 02:51:47 +1000

    feat: install axios and do basic config

---
+++ client/src/request/axios.js
@@ -0,0 +1,4 @@
+import axios from "axios";
+const instance = axios.create({ baseURL: "http://localhost:80" });
+instance.defaults.headers.common["Content-Type"] = "multipart/form-data";
+export default instance;
